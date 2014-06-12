04357bda00a24c9a56cf9d0aff0314999d7c9f12
Andrew Minion <andrew@andrewrminion.com>
2014-06-12 12:30:48 -0400

    SQL query - use variables and prepared query
    delete all manual queries
    replace with one prepared query and add variables

--- Export MySQL to TSV.sql
+++ Export MySQL to TSV.sql
@@ -1,70 +1,24 @@
-# Tweak as needed for your `MediaTypeRefID` values
+# Set the variables and run this query as many times as you have MediaTypes in Librarian’s Edge
 
-# Books
-SELECT * INTO OUTFILE '/tmp/LibEdge/books.txt'
-FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\r\n'
-FROM `Media` WHERE `MediaTypeRefID` LIKE '1';
+SET @KohaItemType = 'BOOKS';	# The code for the Item Type you have (or plan to) created in Koha
+SET @MediaTypeCode = 'Book';	# The existing MediaTypeCode in Librarian’s Edge
+SET @Filename = "/books.txt";	# Filename of your export file
+SET @OutputFile = CONCAT('/tmp/LibEdge/',@Filename); # Path to your export file
 
-# Audio cassettes/CDs
-SELECT * INTO OUTFILE '/tmp/LibEdge/audio.txt'
-FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\r\n'
-FROM `Media` WHERE `MediaTypeRefID` LIKE '3';
+# Prepare the query
+SET @Query = CONCAT("SELECT *,
+	@KohaItemType as KohaItemType,
+	# concatenates author names
+	RTRIM(CONCAT(`AuthorLastName`, ', ', `AuthorFirstName`, ' ', `AuthorMiddleName`)) as AuthorFullName,
+	# concatenates call number
+	RTRIM(REPLACE(CONCAT_WS(' ',`CallNum1`, `CallNum2`, `CallNum3`, `CallNum4`, `CallNum5`),'  ',' ')) as CallNumFull
+	INTO OUTFILE '",@Outputfile,"'
+		FIELDS TERMINATED BY \'\t\' LINES TERMINATED BY \'\r\n\'
+	FROM Media
+	LEFT JOIN `MediaTypes` ON `Media`.`MediaTypeRefID` = `MediaTypes`.`MediaTypeRefID`
+	WHERE `MediaTypeCode` LIKE @MediaTypeCode");
 
-# CDs
-SELECT * INTO OUTFILE '/tmp/LibEdge/CDs.txt'
-FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\r\n'
-FROM `Media` WHERE `MediaTypeRefID` LIKE '5';
-
-# Flannelgraph
-SELECT * INTO OUTFILE '/tmp/LibEdge/flannelgraph.txt'
-FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\r\n'
-FROM `Media` WHERE `MediaTypeRefID` LIKE '4';
-
-# Flashcards
-SELECT * INTO OUTFILE '/tmp/LibEdge/flashcards.txt'
-FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\r\n'
-FROM `Media` WHERE `MediaTypeRefID` LIKE '6';
-
-# Videos
-SELECT * INTO OUTFILE '/tmp/LibEdge/videos.txt'
-FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\r\n'
-FROM `Media` WHERE `MediaTypeRefID` LIKE '7';
-
-# Periodicals
-SELECT * INTO OUTFILE '/tmp/LibEdge/periodicals.txt'
-FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\r\n'
-FROM `Media` WHERE `MediaTypeRefID` LIKE '9';
-
-# Archives
-SELECT * INTO OUTFILE '/tmp/LibEdge/archives.txt'
-FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\r\n'
-FROM `Media` WHERE `MediaTypeRefID` LIKE '12';
-
-# Pamphlets
-SELECT * INTO OUTFILE '/tmp/LibEdge/pamphlets.txt'
-FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\r\n'
-FROM `Media` WHERE `MediaTypeRefID` LIKE '13';
-
-# Missionary Stories
-SELECT * INTO OUTFILE '/tmp/LibEdge/missionary stories.txt'
-FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\r\n'
-FROM `Media` WHERE `MediaTypeRefID` LIKE '14';
-
-
-# Donated books
-SELECT * INTO OUTFILE '/tmp/LibEdge/donated-John Larrabee.txt'
-FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\r\n'
-FROM `Media` WHERE `MediaTypeRefID` LIKE '15';
-SELECT * INTO OUTFILE '/tmp/LibEdge/donated-TD Nichols.txt'
-FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\r\n'
-FROM `Media` WHERE `MediaTypeRefID` LIKE '8';
-
-
-# Reserves
-SELECT * INTO OUTFILE '/tmp/LibEdge/reserve-checkout.txt'
-FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\r\n'
-FROM `Media` WHERE `MediaTypeRefID` LIKE '10';
-# Missionary Stories
-SELECT * INTO OUTFILE '/tmp/LibEdge/reserve-non-checkout.txt'
-FIELDS TERMINATED BY '\t' LINES TERMINATED BY '\r\n'
-FROM `Media` WHERE `MediaTypeRefID` LIKE '11';
+# Run the query
+PREPARE QueryResult from @Query;
+EXECUTE QueryResult;
+DEALLOCATE PREPARE QueryResult;
--- ReadMe.md
+++ ReadMe.md
@@ -4,12 +4,26 @@ Export existing library data from Librarian’s Edge to [Koha](http://koha-commu
 #Process
 Note: these steps assume you’re working on Mac OS X (MySQL, command line) and Windows 7 Pro (MarcEdit). You may need to adjust based on your operating systems.
 
+##1. Getting the Data
 1. Connect to MSSQL server containing Librarian’s Edge data, using [Navicat](http://www.navicat.com/products/navicat-for-sqlserver) or another MSSQL client.
 2. Export at least the `Media` and `MediaTypes` tables as CSV, preferably with column headings.
 3. Create a MySQL database, use the [Media table syntax](Media table syntax.sql) SQL file to set up the tables, then import the CSV files to those tables.
-4. Check the media types in your installation and tweak the export queries as needed.
-5. Export using queries in the [Export MySQL to TSV](Export MySQL to TSV.sql) file.
-6. Using [MarcEdit’s](http://marcedit.reeset.net) Delimited Text Translator tool, convert the `.txt` files to MRK format, using the [MarcEdit template](MarcEdit template.mrd) for field mappings.
-7. Using a text editor, open the `.mrk` files, search using the regex `\=100  1#\$a(?:(.*?)\$a)(?:(.*?)\$a)?`, and replace with `=100  1#$a$1$2` to fix the author’s name (I didn’t have time to put together a shell script).
-8. Using [MarcEdit’s](http://marcedit.reeset.net) MarcMaker tool, convert the `.mrk` files to MRC format.
-9. Set up the categories in [Koha](http://koha-community.org/) and import the `.mrc` files.
+
+##2. Tweaking the Data
+1. Set up the [item types](http://manual.koha-community.org/3.6/en/basicparams.html) in your Koha installation and make note of the item type codes.
+2. Note the media types in your Librarian’s Edge installation and tweak the [export queries](Export MySQL to TSV.sql) file as needed.
+3. Export using queries in the [Export MySQL to TSV](Export MySQL to TSV.sql) file.
+
+##3. Converting the Data
+1. Using [MarcEdit’s](http://marcedit.reeset.net) Delimited Text Translator tool, convert the `.txt` files to MRK format, using the [MarcEdit template](MarcEdit template.mrd) for field mappings.
+2. Using [MarcEdit’s](http://marcedit.reeset.net) MarcMaker tool, convert the `.mrk` files to MRC format.
+3. Set up the categories in [Koha](http://koha-community.org/) and import the `.mrc` files.
+
+##4. Importing the Data
+1. Import to Koha
+
+
+
+
+952$o: Call number field
+952$y: Koha item type (must be already set)
